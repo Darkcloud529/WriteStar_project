4b8a062c75511996acc368605bad2417e7b6ade7
이상민 <88306194+Darkcloud529@users.noreply.github.com>
2022-01-02 22:15:51 +0900

    Add files via upload

---
+++ WriteStar/src/main/java/com/writestar/controller/CommonController.java
@@ -0,0 +1,36 @@
+package com.writestar.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class CommonController {
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth, Model model) {
+		log.info("access denied: " + auth);
+		model.addAttribute("msg","Access Denied");
+	}
+	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		if(error!= null) {
+			model.addAttribute("error", "로그인이 실패했습니다.");
+		}
+		if(logout!=null) {
+			model.addAttribute("logout", "로그아웃 되었습니다.");
+		}
+	}
+	
+	@GetMapping("/customLogout")
+	public void logoutGET() {
+		log.info("custom logout");
+	}
+	@PostMapping("/customLogout")
+	public void logoutPost() {}
+}
