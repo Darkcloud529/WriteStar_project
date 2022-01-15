67f54472f4abfbfece606a0332de3bbf15c7dc34
이상민 <88306194+Darkcloud529@users.noreply.github.com>
2022-01-15 23:40:54 +0900

    Add files via upload

--- WriteStar/src/main/java/com/writestar/domain/UserVO.java
+++ WriteStar/src/main/java/com/writestar/domain/UserVO.java
@@ -2,12 +2,21 @@ package com.writestar.domain;
 
 import java.util.List;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 @Data
 public class UserVO {
+	@NotEmpty
+	@Email
 	private String email;
+	@NotEmpty
+	@Pattern(regexp="(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}")
 	private String password;
+	@NotEmpty
 	private String nickname;
 	private String user_info;
 	
