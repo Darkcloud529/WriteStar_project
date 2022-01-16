56749f39c7939d810b42d431df891aae666e52dc
이상민 <88306194+Darkcloud529@users.noreply.github.com>
2022-01-16 22:49:45 +0900

    Add files via upload

--- WriteStar/src/main/java/com/writestar/domain/BoardVO.java
+++ WriteStar/src/main/java/com/writestar/domain/BoardVO.java
@@ -7,16 +7,16 @@ import lombok.Data;
 
 @Data
 public class BoardVO {
-	private Long bno;
-	private String title;
-	private String content;
-	private Date regdate;
-	private String address;
-	private String post_type;
-	private String email;
-	private Long hits;
-	
-	private UserVO userVO;
-	private List<BoardAttachVO> attachList;
-	private BoardAttachVO thumbnail; 
+	private Long bno;							// 게시물 번호
+	private String title;						// 게시물 제목
+	private String content;						// 게시물 내용
+	private Date regdate;						// 게시물 작성날짜
+	private String address;						// 게시물 사진 속 주소
+	private String post_type;					// 게시물 공개여부(1:전체공개, 2: 친구공개, 3: 비공개)
+	private String email;						// 게시물 작성자(ID)
+	private Long hits;							// 게시물 조횟수
+		
+	private UserVO userVO;						// 게시물 작성자 정보
+	private List<BoardAttachVO> attachList;		// 게시물 첨부파일 정보 (출력)	 
+	private BoardAttachVO thumbnail; 			// 게시물 첨부파일 조인 결과 (출력)
 }
--- WriteStar/src/main/java/com/writestar/domain/UserVO.java
+++ WriteStar/src/main/java/com/writestar/domain/UserVO.java
@@ -12,13 +12,13 @@ import lombok.Data;
 public class UserVO {
 	@NotEmpty
 	@Email
-	private String email;
+	private String email;					// 회원 아이디
 	@NotEmpty
 	@Pattern(regexp="(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}")
-	private String password;
+	private String password;				// 회원 비밀번호
 	@NotEmpty
-	private String nickname;
-	private String user_info;
+	private String nickname;				// 회원 닉네임
+	private String user_info;				// 회원 프로필 정보
 	
-	private List<BoardAttachVO> attachList;
+	private List<BoardAttachVO> attachList; // 회원이 가지고 있는 첨부파일 정보
 }
